83a03de9bfb5051c3b12a58051301d79a0888ba1
LucyferMS <LucyferMS@github.com>
2021-01-19 20:05:09 +0100

    Added Generator v1

--- src/main/java/pl/trollcraft/Skyblock/generator/CreateIsland.java
+++ src/main/java/pl/trollcraft/Skyblock/generator/CreateIsland.java
@@ -6,18 +6,27 @@ import pl.trollcraft.Skyblock.essentials.ConfigUtils;
 
 public class CreateIsland {
 
-    int id;
-    double x;
-    double y;
-    double z;
-    int distance;
-    int maxSize;
-    int goNext;
+    private static int id;
+    private static String world;
+    private static double x;
+    private static double y;
+    private static double z;
+    private static int distance;
+    private static int maxSize;
+    private static int goNext;
+    private static int last;
+    private static int check;
+    private static boolean isAv;
 
     private static void getNextIsland(){
         YamlConfiguration configuration = ConfigUtils.load("nextisland.yml", Main.getInstance());
 
+        id = configuration.getInt("nextIsland.id");
 
+        world = configuration.getString("nextIsland.world");
+        x = configuration.getDouble("nextIsland.x");
+        y = configuration.getDouble("nextIsland.y");
+        z = configuration.getDouble("nextIsland.z");
 
     }
 
