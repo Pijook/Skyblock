c83c8ccb26f6f9d756b490a1eeacaaf1ec027a13
Pijook <lordbaby330@gmail.com>
2021-01-19 19:48:36 +0100

    Added player functions

--- src/main/java/pl/trollcraft/Skyblock/listeners/JoinListener.java
+++ src/main/java/pl/trollcraft/Skyblock/listeners/JoinListener.java
@@ -4,11 +4,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import pl.trollcraft.Skyblock.skyblockplayer.SkyblockPlayers;
 
 public class JoinListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event){
         Player player = event.getPlayer();
+
+        SkyblockPlayers.loadPlayer(player.getName());
     }
 }
--- src/main/java/pl/trollcraft/Skyblock/skyblockplayer/SkyblockPlayer.java
+++ src/main/java/pl/trollcraft/Skyblock/skyblockplayer/SkyblockPlayer.java
@@ -2,17 +2,35 @@ package pl.trollcraft.Skyblock.skyblockplayer;
 
 public class SkyblockPlayer {
 
-    private int islandID;
+    private String islandID;
+    private String coopIslandID;
 
-    public SkyblockPlayer(int islandID){
+    public SkyblockPlayer(String islandID, String coopIslandID){
         this.islandID = islandID;
+        this.coopIslandID = coopIslandID;
     }
 
-    public int getIslandID() {
+    public String getIslandID() {
         return islandID;
     }
 
-    public void setIslandID(int islandID) {
+    public void setIslandID(String islandID) {
         this.islandID = islandID;
+    }
+
+    public String getCoopIslandID() {
+        return coopIslandID;
+    }
+
+    public void setCoopIslandID(String coopIslandID) {
+        this.coopIslandID = coopIslandID;
+    }
+
+    public boolean hasIsland(){
+        return islandID != null;
+    }
+
+    public boolean hasCOOP(){
+        return coopIslandID != null;
     }
 }
