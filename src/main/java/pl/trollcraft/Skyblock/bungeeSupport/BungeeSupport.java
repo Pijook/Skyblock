81f43bf0a7e9c837328cfdc17b48e198d60b131a
Pijook <lordbaby330@gmail.com>
2021-02-16 22:16:49 +0100

    A lot of fixes

--- src/main/java/pl/trollcraft/Skyblock/bungeeSupport/BungeeSupport.java
+++ src/main/java/pl/trollcraft/Skyblock/bungeeSupport/BungeeSupport.java
@@ -12,6 +12,7 @@ import pl.trollcraft.Skyblock.generator.CreateIsland;
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.util.UUID;
 
 public class BungeeSupport {
 
