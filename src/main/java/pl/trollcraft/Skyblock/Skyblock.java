6fdbdbfa4cc2a53003dc74217410c767cf9311a1
Pijook <lordbaby330@gmail.com>
2021-04-04 13:38:28 +0200

    IslandSecurityListener

--- src/main/java/pl/trollcraft/Skyblock/Skyblock.java
+++ src/main/java/pl/trollcraft/Skyblock/Skyblock.java
@@ -96,10 +96,10 @@ public class Skyblock extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new BlockPlaceListener(), this);
         getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
         getServer().getPluginManager().registerEvents(new CommandListener(), this);
-        getServer().getPluginManager().registerEvents(new ChatListener(), this);
         getServer().getPluginManager().registerEvents(new RespawnListener(), this);
         getServer().getPluginManager().registerEvents(new InteractListener(), this);
         getServer().getPluginManager().registerEvents(new EntityDamageListener(), this);
+        getServer().getPluginManager().registerEvents(new IslandSaveListener(), this);
         //Custom Events
         getServer().getPluginManager().registerEvents(new PlayerLoadListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerSaveListener(), this);
---
+++ src/main/java/pl/trollcraft/Skyblock/listeners/IslandSecurityListener.java
@@ -0,0 +1,51 @@
+package pl.trollcraft.Skyblock.listeners;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+import pl.trollcraft.Skyblock.Skyblock;
+import pl.trollcraft.Skyblock.island.IslandsController;
+import pl.trollcraft.Skyblock.skyblockplayer.SkyblockPlayerController;
+
+public class IslandSecurityListener implements Listener {
+
+    private static SkyblockPlayerController skyblockPlayerController = Skyblock.getSkyblockPlayerController();
+    private static IslandsController islandsController = Skyblock.getIslandsController();
+
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onPickup(PlayerPickupItemEvent event){
+
+        Player player = event.getPlayer();
+        if(!islandsController.isPlayerOnHisIsland(player)){
+            event.setCancelled(true);
+        }
+
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onEntityDamage(EntityDamageByEntityEvent event){
+        if(event.getDamager().getType().equals(EntityType.PLAYER)){
+            Player player = (Player) event.getDamager();
+
+            if(!islandsController.isPlayerOnHisIsland(player)){
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onInteract(PlayerInteractEvent event){
+
+        if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
+            event.setCancelled(true);
+        }
+
+    }
+}
