fed84218392ee03c9e27097a97ea7d9a1265bfa2
Pijook <lordbaby330@gmail.com>
2021-04-16 14:07:38 +0200

    Few fixes

--- src/main/java/pl/trollcraft/Skyblock/listeners/IslandSecurityListener.java
+++ src/main/java/pl/trollcraft/Skyblock/listeners/IslandSecurityListener.java
@@ -10,6 +10,7 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import pl.trollcraft.Skyblock.Skyblock;
+import pl.trollcraft.Skyblock.Storage;
 import pl.trollcraft.Skyblock.island.IslandsController;
 import pl.trollcraft.Skyblock.skyblockplayer.SkyblockPlayerController;
 
@@ -22,6 +23,10 @@ public class IslandSecurityListener implements Listener {
     @EventHandler(priority = EventPriority.LOW)
     public void onPickup(PlayerPickupItemEvent event){
 
+        if(Storage.serverName.equalsIgnoreCase("sblobby")){
+            return;
+        }
+
         Player player = event.getPlayer();
         if(!islandsController.isPlayerOnHisIsland(player)){
             event.setCancelled(true);
@@ -31,6 +36,10 @@ public class IslandSecurityListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOW)
     public void onEntityDamage(EntityDamageByEntityEvent event){
+        if(Storage.serverName.equalsIgnoreCase("sblobby")){
+            return;
+        }
+
         if(event.getDamager().getType().equals(EntityType.PLAYER)){
             Player player = (Player) event.getDamager();
 
@@ -42,6 +51,10 @@ public class IslandSecurityListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOW)
     public void onInteract(PlayerInteractEvent event){
+
+        if(Storage.serverName.equalsIgnoreCase("sblobby")){
+            return;
+        }
 
         if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK)){
             event.setCancelled(true);
