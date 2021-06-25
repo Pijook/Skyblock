d4dd20bd00b39a4e7fc3eda237746372f9d610cb
Pijook <lordbaby330@gmail.com>
2021-06-25 02:59:52 +0200

    Fixed pistons

--- src/main/java/pl/trollcraft/Skyblock/listeners/BlockPistonListener.java
+++ src/main/java/pl/trollcraft/Skyblock/listeners/BlockPistonListener.java
@@ -1,13 +1,10 @@
 package pl.trollcraft.Skyblock.listeners;
 
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPistonEvent;
 import org.bukkit.event.block.BlockPistonExtendEvent;
-import org.bukkit.event.block.BlockPistonRetractEvent;
 import pl.trollcraft.Skyblock.Skyblock;
 import pl.trollcraft.Skyblock.island.Island;
 import pl.trollcraft.Skyblock.island.IslandsController;
@@ -16,27 +13,22 @@ public class BlockPistonListener implements Listener {
 
     private final IslandsController islandsController = Skyblock.getIslandsController();
 
-    @EventHandler(priority = EventPriority.LOW)
+    @EventHandler(priority = EventPriority.LOWEST)
     public void onPistonMove(BlockPistonExtendEvent event){
-        Block block = event.getBlock();
-        Island island = islandsController.getIslandByLocation(block.getLocation());
 
-        if(island == null){
+        for(Block block : event.getBlocks()){
+            Island island = islandsController.getIslandByLocation(block.getLocation());
+            if(island == null){
+                event.setCancelled(true);
+                break;
+            }
+        }
+
+        /*if(island == null){
             block.setType(Material.AIR);
             event.setCancelled(true);
             return;
-        }
+        }*/
     }
 
-    @EventHandler(priority = EventPriority.LOW)
-    public void onPistonMove(BlockPistonRetractEvent event){
-        Block block = event.getBlock();
-        Island island = islandsController.getIslandByLocation(block.getLocation());
-
-        if(island == null){
-            block.setType(Material.AIR);
-            event.setCancelled(true);
-            return;
-        }
-    }
 }
